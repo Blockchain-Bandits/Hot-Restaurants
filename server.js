0ac30025072baf27ae031827553c8a019f105dd4
Mus-tard <erichan210hh@gmail.com>
2017-11-11 10:42:40 -0800

    added port listener

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ var app = express();
 var PORT = 3000;
 var PORT = process.env.PORT || 3000;
 
-var arr = [];
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
